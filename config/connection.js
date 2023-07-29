771d98729697d16382dae122d93321708a7458f8
Munshid <muhammadmunshid200@gmail.com>
2023-07-29 12:23:20 +0530

    mongoose connection again changed

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 module.exports.connect = function () {
-    mongoose.connect('mongodb://mongodb+srv://munshid:munshid123@cluster0.fyiocsw.mongodb.net/job-portal')
+    mongoose.connect('mongodb+srv://munshid:munshid123@cluster0.fyiocsw.mongodb.net/job-portal?retryWrites=true&w=majority')
     console.log('mongoose connected');
 }
